6586eccd3492809d5d4552a9e13b21a61cb5acb3
tiny-hippo <muesim@physik.uzh.ch>
2022-03-04 11:34:54 +0100

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tinyeos",
-    version="2.0",
+    version="1.1",
     description="equations of state and opacities for planets",
     url="",
     author="Simon Müller",
