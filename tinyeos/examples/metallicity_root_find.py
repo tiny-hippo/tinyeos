5291e9e328c439150de853c09c6f62fed670baf0
tiny-hippo <simonmueller.ch@gmail.com>
2023-02-22 11:09:38 +0100

    Update metallicity_root_find.py

--- tinyeos/examples/metallicity_root_find.py
+++ tinyeos/examples/metallicity_root_find.py
@@ -4,6 +4,7 @@ heavy-element profile for a given (logT, logRho, LogP).
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from numpy.typing import NDArray
 from scipy.optimize import brentq
 from tinyeos.tinypteos import TinyPT
 from tinyeos.support import get_X
@@ -13,10 +14,10 @@ mpl.rcParams["lines.linewidth"] = 2.5
 mpl.rcParams["font.size"] = 12
 
 
-def get_Z(which_heavy, logT, logP, logRho) -> np.ndarray:
+def get_z(which_heavy, logT, logP, logRho) -> NDArray:
     """Attempt to find the root in Z with the
     brentq algorithm in the interval [min_Z, max_Z] for a given
-    logT, logP and logRho profile. This assumes 
+    logT, logP and logRho profile. This assumes
     proto-solar hydrogen-helium ratio.
 
     Args:
@@ -29,14 +30,10 @@ def get_Z(which_heavy, logT, logP, logRho) -> np.ndarray:
     Returns:
         z_root_pt (np.ndarray): Heavy-element profile
     """
-    # upper and lower bracket for brentq
-    min_Z = 0
-    max_Z = 1
-
     # initiate the equation of state
     tpt = TinyPT(which_heavy, which_hhe="cms", include_hhe_interactions=False)
 
-    def func_PT(Z, logT, logP, logRho) -> float:
+    def func_pt(Z, logT, logP, logRho) -> float:
         """Support function for the heavy-element fraction
         root finding.
 
@@ -54,14 +51,25 @@ def get_Z(which_heavy, logT, logP, logRho) -> np.ndarray:
         eos_result = tpt.evaluate(logT, logP, X, Z)
         return logRho - eos_result[i_logRho]
 
+    # upper and lower bracket for brentq
+    min_Z = 0
+    max_Z = 1
     z_root_pt = np.zeros_like(logP)
     for i in range(logP.size):
-        try:
-            sol = brentq(func_PT, a=min_Z, b=max_Z, args=(logT[i], logP[i], logRho[i]))
-            z_root_pt[i] = sol
-        except ValueError:
-            # func_PT(min_Z) and func_PT(max_Z) have the same signs
-            z_root_pt[i] = np.nan
+        # check that the signs are different at the brackets
+        f_lower = func_pt(min_Z, logT[i], logP[i], logRho[i])
+        f_upper = func_pt(max_Z, logT[i], logP[i], logRho[i])
+        if np.sign(f_lower) == np.sign(f_upper):
+            # no root within brackets
+            sol = np.nan
+        else:
+            sol = brentq(
+                func_pt,
+                a=min_Z,
+                b=max_Z,
+                args=(logT[i], logP[i], logRho[i])
+                )
+        z_root_pt[i] = sol
     return z_root_pt
 
 
@@ -76,14 +84,18 @@ logT = np.log10(planet_profile[:, 2])
 logRho = np.log10(planet_profile[:, 3])
 
 # get the heavy-element profiles and plot them
-z_water = get_Z("h2o", logT, logP, logRho)
-z_rock = get_Z("sio2", logT, logP, logRho)
-z_mix = get_Z("mixture", logT, logP, logRho)
+z_water = get_z("h2o", logT, logP, logRho)
+z_rock = get_z("sio2", logT, logP, logRho)
+z_mix = get_z("mixture", logT, logP, logRho)
+z_co = get_z("co", logT, logP, logRho)
+z_fe = get_z("fe", logT, logP, logRho)
 
 fig, ax = plt.subplots(1, 1, figsize=(6, 4))
-ax.plot(R, z_water, color="tab:purple", label=r"$Z_{H_20}$")
-ax.plot(R, z_rock, color="tab:grey", label=r"$Z_{SiO_2}$")
-ax.plot(R, z_mix, color="tab:pink", label=r"$Z_{mix}$")
+ax.plot(R, z_water, label=r"H$_2$0")
+ax.plot(R, z_co, label=r"CO")
+ax.plot(R, z_rock, label=r"SiO$_2$")
+ax.plot(R, z_mix, label=r"50-50 H$_2$O-SiO$_2$")
+ax.plot(R, z_fe, label=r"Fe")
 ax.set_xlim(left=0, right=4)
 ax.set_ylim(bottom=0, top=1)
 ax.set_xlabel(r"Radius [R$_\oplus$]")
