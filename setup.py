fa1ee9f87d2c6e498e4f5aa75d781e6599640fbd
Simon <simonmueller.ch@gmail.com>
2024-02-20 15:30:39 +0100

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tinyeos",
-    version="1.4.8",
+    version="1.5.0",
     description="equations of state and opacities for planets",
     url="",
     author="Simon Müller",
