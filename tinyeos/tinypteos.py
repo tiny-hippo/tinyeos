5878f4d64993312d8f6e60222768545d34a24b37
tiny-hippo <simonmueller.ch@gmail.com>
2025-01-17 13:57:40 +0100

    Fix Yoda conditions

--- tinyeos/tinypteos.py
+++ tinyeos/tinypteos.py
@@ -327,7 +327,7 @@ class TinyPT(InterpolantsBuilder):
             if np.any(self.X_close) and self.include_hhe_interactions:
                 logRho_x = self.interpPT_logRho_x(logT, logP, grid=False)
                 logRho_x_eff = self.interpPT_logRho_x_eff(logT, logP, grid=False)
-                i = np.logical_and(X > tiny_val, Y > tiny_val)
+                i = np.logical_and(tiny_val < X, tiny_val < Y)
                 logRho_x[i] = logRho_x_eff[i]
                 logRho_y = self.interpPT_logRho_y(logT, logP, grid=False)
                 logRho_z = self.interpPT_logRho_z(logT, logP, grid=False)
