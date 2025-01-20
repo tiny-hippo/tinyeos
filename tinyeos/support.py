858f6043f4daa78b20f221aeedec70171ba72518
tiny-hippo <simonmueller.ch@gmail.com>
2025-01-20 13:05:54 +0100

    Add __get_weights function
    This removes the scikit-learn dependence.

--- tinyeos/support.py
+++ tinyeos/support.py
@@ -5,7 +5,6 @@ from numpy.typing import ArrayLike, NDArray
 from scipy.interpolate import UnivariateSpline
 from scipy.interpolate._interpnd import NDInterpolatorBase, _ndim_coords_from_arrays
 from scipy.spatial import cKDTree
-from sklearn.neighbors._base import _get_weights
 
 from tinyeos.definitions import eos_num_vals, eps1, eps2, tiny_val
 
@@ -74,6 +73,58 @@ class NearestND(NDInterpolatorBase):
                 raise NotImplementedError(msg)
 
             return np.average(self.values[i], axis=which_axis, weights=weight_matrix)
+
+    @staticmethod
+    def __get_weights(dist, weights):
+        """
+        Get the weights from an array of distances and a parameter ``weights``.
+
+        Assume weights have already been validated.
+
+        Note:
+        This function is copied from scikit-learn, specifically from
+        sklearn.neighbors._base. It is used to calculate the weight
+        maxtrix in the nearest neighbor interpolation.
+
+        Parameters
+        ----------
+        dist : ndarray
+            The input distances.
+
+        weights : {'uniform', 'distance'}, callable or None
+            The kind of weighting used.
+
+        Returns
+        -------
+        weights_arr : array of the same shape as ``dist``
+            If ``weights == 'uniform'``, then returns None.
+        """
+        if weights in (None, "uniform"):
+            return None
+
+        if weights == "distance":
+            # if user attempts to classify a point that was zero distance from one
+            # or more training points, those training points are weighted as 1.0
+            # and the other points as 0.0
+            if dist.dtype is np.dtype(object):
+                for point_dist_i, point_dist in enumerate(dist):
+                    # check if point_dist is iterable
+                    # (ex: RadiusNeighborClassifier.predict may set an element of
+                    # dist to 1e-6 to represent an 'outlier')
+                    if hasattr(point_dist, "__contains__") and 0.0 in point_dist:
+                        dist[point_dist_i] = point_dist == 0.0
+                    else:
+                        dist[point_dist_i] = 1.0 / point_dist
+            else:
+                with np.errstate(divide="ignore"):
+                    dist = 1.0 / dist
+                inf_mask = np.isinf(dist)
+                inf_row = np.any(inf_mask, axis=1)
+                dist[inf_row] = inf_mask[inf_row]
+            return dist
+
+        if callable(weights):
+            return weights(dist)
 
 
 def get_X(Z: ArrayLike) -> ArrayLike:
