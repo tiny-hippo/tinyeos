7720786f2b99e52d5adbf5e47d6670476105513e
Simon <simonmueller.ch@gmail.com>
2025-03-12 10:49:25 +0100

    Update __init__.py

--- tinyeos/__init__.py
+++ tinyeos/__init__.py
@@ -3,3 +3,5 @@ from tinyeos.tinydtwrapper import TinyDTWrapper
 from tinyeos.tinykap import TinyOpacity
 from tinyeos.tinypteos import TinyPT
 from tinyeos.tinyptmixture import TinyPTMixture
+
+__all__ = ["TinyDT", "TinyDTWrapper", "TinyOpacity", "TinyPT", "TinyPTMixture"]
